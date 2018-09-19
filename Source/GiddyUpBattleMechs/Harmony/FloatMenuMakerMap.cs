de1edf7405db472c8ad156914f05404b399ee63f
Roel <roelheirman@gmail.com>
2018-09-19 10:24:26 +0200

    added float menu option

---
+++ Source/GiddyUpBattleMechs/Harmony/FloatMenuMakerMap.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using RimWorld;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+using GiddyUpCore;
+using GiddyUpCore.Jobs;
+using GiddyUpCore.Utilities;
+using WhatTheHack;
+
+namespace GiddyUpBattleMechs.Harmony
+{
+    [HarmonyPatch(typeof(FloatMenuMakerMap), "AddDraftedOrders")]
+    [HarmonyPatch(new Type[] { typeof(Vector3), typeof(Pawn), typeof(List<FloatMenuOption>) })]
+    static class FloatMenuMakerMap_AddDraftedOrders
+    {
+        static void Postfix(Vector3 clickPos, Pawn pawn, List<FloatMenuOption> opts)
+        {
+            if (pawn.IsHacked())
+            {
+                GUC_FloatMenuUtility.AddMountingOptions(clickPos, pawn, opts);
+            }
+        }
+    }
+}
