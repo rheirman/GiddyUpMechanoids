4a161ea9c5184ba33964a76a2654961dc9d83b8a
rheirman <roelheirman@gmail.com>
2020-04-19 15:15:01 +0200

    v1.1 support

---
+++ 1.1/Source/GiddyUpMechanoids/Harmony/WTH_IncidentWorker_Raid_TryExecuteWorker.cs
@@ -0,0 +1,64 @@
+﻿using HarmonyLib;
+using WhatTheHack.Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using WhatTheHack;
+using Verse.AI;
+using GiddyUpCore.Jobs;
+using GiddyUpCore.Storage;
+using GiddyUpCore.Utilities;
+using RimWorld;
+
+namespace GiddyUpMechanoids.Harmony
+{
+    [HarmonyPatch(typeof(IncidentWorker_Raid_TryExecuteWorker), "SpawnHackedMechanoids")]
+    class WTH_IncidentWorker_Raid_TryExecuteWorker
+    {
+        static void Postfix(ref IEnumerable<Pawn> __result, IncidentParms parms)
+        {
+            if(parms.raidArrivalMode != PawnsArrivalModeDefOf.EdgeWalkIn)
+            {
+                return;
+            }
+
+            List<Pawn> mechs = __result.ToList().FindAll((Pawn p) => p.IsHacked());
+            List<Pawn> humanlikes = __result.ToList().FindAll((Pawn h) => h.RaceProps.Humanlike && GiddyUpCore.Base.Instance.GetExtendedDataStorage().GetExtendedDataFor(h).mount == null);
+
+            Random r = new Random();
+            foreach(Pawn mech in mechs)
+            {
+                if (Rand.Chance((Base.mountChance)/100f) && Base.IsAllowedInModOptions(mech.def.defName))
+                {
+                    if(humanlikes.Count > 0)
+                    {
+
+                        Pawn rider = humanlikes.Pop();
+                        RideMech(mech, rider);
+                    }
+                }
+            }
+        }
+
+        private static void RideMech(Pawn mech, Pawn rider)
+        {
+            ExtendedPawnData riderData = GiddyUpCore.Base.Instance.GetExtendedDataStorage().GetExtendedDataFor(rider);
+            ExtendedPawnData mechData = GiddyUpCore.Base.Instance.GetExtendedDataStorage().GetExtendedDataFor(mech);
+            riderData.mount = mech;
+            GiddyUpCore.Utilities.TextureUtility.setDrawOffset(riderData);
+            mechData.ownedBy = rider;
+            riderData.owning = mech;
+            mech.health.AddHediff(GU_Mech_DefOf.GU_Mech_GiddyUpModule);
+            if (mech.jobs == null)
+            {
+                mech.jobs = new Pawn_JobTracker(mech);
+            }
+            Job job = new Job(GUC_JobDefOf.Mounted, rider) { count = 1 };
+            mech.jobs.StartJob(job, JobCondition.InterruptForced);
+
+
+        }
+    }
+}
---
+++ 1.1/Source/GiddyUpMechanoids/Harmony/WTH_Recipe_ModifyMechanoid.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using WhatTheHack.Recipes;
+
+namespace GiddyUpMechanoids.Harmony
+{
+    [HarmonyPatch(typeof(Recipe_ModifyMechanoid), "CanApplyOn")]
+    class WTH_Recipe_ModifyMechanoid_CanApplyOn
+    {
+        static void Postfix(Recipe_ModifyMechanoid __instance, Pawn pawn, ref string reason, ref bool __result)
+        {
+            if (__instance.recipe == GU_Mech_DefOf.GU_Mech_InstallGiddyUpModule && !Base.IsAllowedInModOptions(pawn.def.defName))
+            {
+                reason = "GU_BME_Reason_NotAllowed".Translate();
+                Log.Message("Mech not allowed in mod options");
+                __result = false;
+            }
+        }
+    }
+    [HarmonyPatch(typeof(Recipe_ModifyMechanoid), "IsValidPawn")]
+    class WTH_Recipe_ModifyMechanoid_IsValidPawn
+    {
+        static void Postfix(Recipe_ModifyMechanoid __instance, Pawn pawn, ref bool __result)
+        {
+            if (!__result)
+            {
+                Log.Message("IsValidPawn is false");
+            }
+        }
+    }
+}
